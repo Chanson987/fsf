c17edccce01c0219f7e7b830fcde89c75230d20d
Chanson987 <90367187+Chanson987@users.noreply.github.com>
2022-01-13 10:49:37 +0800

    Add files via upload

---
+++ 195.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main() {
+    int i=0;
+    printf("叫块计祘Α盢穦滦计 \n");
+    scanf("%d", &i);
+    printf("%d", i);
+int main() {
+
+    int input;
+
+    printf("–块计祘Α穦计\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    //please refer page 4 of homework request4
+    while (scanf("%d", &input)) {
+        printf("%d\n", input);
+    }
+}
