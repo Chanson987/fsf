e988d4a40f2a94411529932d9ea3638a947a7c13
Chanson987 <90367187+Chanson987@users.noreply.github.com>
2022-01-13 10:32:28 +0800

    Add files via upload

---
+++ 123.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+
+    printf("請輸入五個數字，以空白隔開。\n");
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
